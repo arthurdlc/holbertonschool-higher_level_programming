cfdefc16003cbae52e76e749c3de075044fcfa4c
arthurdlc <delacour.a1109@gmail.com>
2024-11-19 15:20:37 +0100

    demo 01

---
+++ python-if_else_loop_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from random import randint
+number = randint(-10,10)
+if number < 0 :
+	print("is negative")
+else if  number = 0: 
+	print("is zero")
+else: 
+	print("is positive")
