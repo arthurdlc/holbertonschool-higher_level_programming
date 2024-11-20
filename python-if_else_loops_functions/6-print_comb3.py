4b80cd85fa82351b1896de3b12d0513772a91d60
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 11:18:52 +0100

    demo 20

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:  # Dernière combinaison
-            print(f"{i}{j}")  # Print final sans virgule ni espace
-        elif i < 5:  # Combinaisons pour les premiers chiffres (0 à 4)
-            print(f"{i}{j}", end=", ")
-        else:  # Combinaisons pour les autres chiffres (5 à 8)
+        if i == 8 and j == 9:
+            print(f"{i}{j}")
+        else:
             print(f"{i}{j}", end=", ")
