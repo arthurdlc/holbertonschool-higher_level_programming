d9a0930d82e13bf6a90e0e3608b66597d1c97a73
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 11:05:28 +0100

    demo 20

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):  # Outer loop for the first digit
-    for j in range(i + 1, 10):  # Inner loop for the second digit
-        if i == 8 and j == 9:  # Last combination
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}", end=", ")  # Print with comma and space
+for i in range(10):
+    for j in range(i + 1, 10):
+        print(f"{i}{j}", end=", " if not (i == 8 and j == 9) else "\n")
