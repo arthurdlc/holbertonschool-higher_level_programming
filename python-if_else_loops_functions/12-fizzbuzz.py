c1698ce2cffc3f680e5d7a5fe83219531bb19ede
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 15:10:29 +0100

    cxlwhgfjuceqswhgfriueksuqgtrhvdo

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-for i in range 101:
-    if i%3 == 0:
-        if i%5 == 0:
-            print("FizzBuzz")
-        else:
-	    print("Fizz")
-    elif i%5 == 0:
-        if i%3 == 0:
-            print("FizzBuzz")
-        else:
-            print("Buzz")
+for i in range(101):  # Corrected to use parentheses
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")  # Print FizzBuzz if divisible by both 3 and 5
+    elif i % 3 == 0:
+        print("Fizz")  # Print Fizz if divisible by 3
+    elif i % 5 == 0:
+        print("Buzz")  # Print Buzz if divisible by 5
     else:
-        print(i)
-    print(" ")
+        print(i)  # Print the number if not divisible by 3 or 5
