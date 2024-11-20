d57352df5779d072c3348db956b7668cec12e379
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 15:17:13 +0100

    cxlwhgfjuceqswhgfriueksuqgtrhvdo

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-def fizzbuzz():
-    result = []  # Créer une liste pour stocker les résultats
-    for i in range(1, 101):  # Boucle de 1 à 100
-        if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")  # Ajouter "FizzBuzz" pour les multiples de 3 et 5
-        elif i % 3 == 0:
-            result.append("Fizz")  # Ajouter "Fizz" pour les multiples de 3
-        elif i % 5 == 0:
-            result.append("Buzz")  # Ajouter "Buzz" pour les multiples de 5
-        else:
-            result.append(str(i))  # Ajouter le nombre lui-même s'il n'est pas un multiple
-
-    print(" ".join(result))  # Joindre les résultats avec des espaces et les imprimer
+for i in range(101):  # Corrected to use parentheses
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")  # Print FizzBuzz if divisible by both 3 and 5
+    elif i % 3 == 0:
+        print("Fizz")  # Print Fizz if divisible by 3
+    elif i % 5 == 0:
+        print("Buzz")  # Print Buzz if divisible by 5
+    else:
+        print(i)  # Print the number if not divisible by 3 or 5
