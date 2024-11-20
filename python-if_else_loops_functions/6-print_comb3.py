dcdf119184a99c567c2002fbfe0c61efcd43d4c6
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 11:06:35 +0100

    demo 20

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if not (i == 8 and j == 9) else "\n")
+print(", ".join(f"{i}{j}" for i in range(10) for j in range(i + 1, 10)))
