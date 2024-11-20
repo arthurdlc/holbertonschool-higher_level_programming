3ea333b2ec0c6f0a2acee8a20b3e183067263d5d
arthurdlc <delacour.a1109@gmail.com>
2024-11-20 13:28:16 +0100

    demo jsp

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     result = ""
     for char in str:
-        if 97 <= ord(char) <= 122:  # Check if the character is a lowercase letter
-            result += chr(ord(char) - 32)  # Convert to uppercase by subtracting 32 from ASCII value
+        if 97 <= ord(char) <= 122:  # Check if char is lowercase
+            result += chr(ord(char) - 32)  # Convert to uppercase
         else:
-            result += char  # Keep the character unchanged if it's not lowercase
-    print("{}".format(result))  # Print the result using string format
+            result += char  # Keep unchanged if not lowercase
+    print("{}".format(result))  # Print the result
