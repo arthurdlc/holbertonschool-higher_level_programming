bca2c936d050ad1fb27b4dc7650985c067ad3699
arthurdlc <112930152+arthurdlc@users.noreply.github.com>
2024-11-20 11:10:07 +0100

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end="\n" if i == 8 and j == 9 else ", ")
+        if i == 8 and j == 9:  # Dernière combinaison
+            print(f"{i}{j}")
+        elif i < 8 or j < 9:  # Toutes les combinaisons sauf la dernière
+            print(f"{i}{j}", end=", ")
