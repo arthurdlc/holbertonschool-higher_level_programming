006dc392616251bd3d6c9399c257780ae5d313d3
arthurdlc <delacour.a1109@gmail.com>
2024-11-21 10:14:09 +0100

    jhieiuhfiesrf

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This is my first class."""
+    def __init__(self, size):
+        """ Initialise un instace de la classe Square avec un attribut privé """
+        self.__size = size # Attribut privé
