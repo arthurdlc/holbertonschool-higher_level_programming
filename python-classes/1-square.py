ca61877ac5e744b1dd4bb3b3d3e7cea1799a4e0a
arthurdlc <delacour.a1109@gmail.com>
2024-11-21 10:32:39 +0100

    ecgcge

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,6 +4,9 @@
 
 class Square:
     """This is my first class."""
+
     def __init__(self, size):
-        """ Initialise un instace de la classe Square avec un attribut privé """
-        self.__size = size # Attribut privé
+        """
+        Initialize an instance of the Square class with a private attribute.
+        """
+        self.__size = size  # Private attribute
