c57c1143cc39a423def562e0b65461c841b5baf2
arthurdlc <delacour.a1109@gmail.com>
2024-11-21 13:41:05 +0100

    demo 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """A class that defines a square with size and calculates its area."""
+
+    def __init__(self, size: int = 0):
+        """
+        Initialize an instance of the Square class.
+
+        Args:
+            size (int): The size of the square's sides. Must be >= 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self) -> int:
+        """int: Get or set the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self) -> int:
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
