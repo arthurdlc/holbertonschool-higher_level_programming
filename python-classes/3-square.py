5e74384c8d5e88d50a32a722372ea3df4e6d43c0
arthurdlc <delacour.a1109@gmail.com>
2024-11-21 11:20:13 +0100

    sihgcasifq

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This is my first class."""
+
+    def __init__(self, size=0):
+        """
+        Initialize an instance of the Square class with a private attribute.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Equivalent d'un this en java/php
+
+    def area(sefl):
+        int self = self.__size
+        int area = self ** 2
+        return area
