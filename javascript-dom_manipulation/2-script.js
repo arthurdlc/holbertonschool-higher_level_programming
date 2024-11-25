43fb33bd63259b8631fc2adec843152d39b49075
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 15:29:52 +0100

    cdcdcdcd

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,2 +1,5 @@
-const class_red = document.getElementsByClassName('red_header');
-header.class = 'red'
+// ce code est fait pour changer la classe du header quand l'utilisateur clique dessus
+const header = document.getElementById('header'); // ou document.querySelector('.header')
+const class_red = document.getElementsByClassName('red_header'); // la constant retient la classe red
+
+header.classList.add('red'); // on ajoute la classe red au header pass avec un = mais avec la methode add
