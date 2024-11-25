49037b7bb81153f33897791111f0bfbafc06a87e
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 13:48:18 +0100

    cxeizscs

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- script that creates a table called second_table in the current database
 CREATE TABLE if not exists second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
