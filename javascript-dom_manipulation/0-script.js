a36f163fb859be65c77f706510282c8627e7d05b
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 14:44:23 +0100

    dede

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,2 +1,3 @@
-<script>
-</script>
+// on met a jour la couleur de la page et on stocke la couleur dans une constante
+const header = document.querySelector('header');
+header.style.color = '#FF0000';    
---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,3 @@
+// on met a jour la couleur de la page et on stocke la couleur dans une constante
+const redHeader = document.getElementById('red_header').style.color = '#FF0000'; // pour recup un truc dans le document c'est document.getelementById
+header.style.color = '#FF0000';    
