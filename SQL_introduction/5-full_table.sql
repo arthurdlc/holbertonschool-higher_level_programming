d33bd5bfe7b84eafb5f7f9495b4e4023eaa829c1
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 13:06:24 +0100

    ddefbq

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,7 @@
 -- script that updates the score of Bob to 10 in the table second
 UPDATE second SET score = 10 WHERE name = 'Bob';
+
+-- Get the table structure without using DESCRIBE or EXPLAIN
+SELECT TABLE_NAME, CREATE_TABLE 
+FROM information_schema.TABLES 
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
