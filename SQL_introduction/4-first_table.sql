6203768818d08e88d0ce506de7296e93875fc8d0
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-25 11:06:39 +0100

    ezkzhsbqfvkqs

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- script that creates the table first_table in the current database
-CREATE TABLES IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT PRIMARY KEY, name VARCHAR(255));
