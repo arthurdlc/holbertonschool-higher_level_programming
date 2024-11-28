82e1e13d704f3121c3acfb5bb82ec7dd95a916cd
Thalie DELACOUR <delacour.a1109@ik.me>
2024-11-28 11:10:19 +0100

    ddcs

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
+// Récupération des arguments depuis la ligne de commande
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath) {
-  console.error('Error: No file specified');
+// Vérifier si les deux arguments nécessaires sont fournis
+if (!filePath || content === undefined) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
-// lire les contenue des fichier sous le format utf8
-fs.writeFile(filePath, 'utf8', (err) => {
+// Écriture dans le fichier en utf-8
+fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
+    process.exit(1);
   }
 });
